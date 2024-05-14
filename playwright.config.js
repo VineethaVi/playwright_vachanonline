53186e2ab4ea6dd2235510da267c73ac0dd4fd43
SeemaNandihal <seemacornel@gmail.com>
2024-05-14 10:25:27 +0530

    allure package added

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,7 @@
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test');
+const { allure } = require('allure-playwright');
+import { testPlanFilter } from "allure-playwright/dist/testplan";
 
 /**
  * Read environment variables from file.
@@ -21,7 +23,9 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  grep: testPlanFilter(),
+  reporter: [["line"], ["allure-playwright"],['html']],
+ 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
